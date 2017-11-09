5f4d4450c58d10ec7e12966bfa71bdea9f4751d0
unit-sport <contact@unit-sport.fr>
2017-11-09 15:25:05 +0100

    test collé glissé

---
+++ site_web_complet/source/javascripts/app.js
@@ -0,0 +1,19 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require_tree .
+
+function verifMail(champ)
+{
+   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   if(!regex.test(champ.value))
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
